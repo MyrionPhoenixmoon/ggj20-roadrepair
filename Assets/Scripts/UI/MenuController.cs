3bf00af848a6ee1209398c4c1382ac934988238f
Mark Zeman <zemanmark@hotmail.com>
2020-02-01 17:26:39 +0100

    Title Screen first draft with players ready and transition to GameScene

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+public GameObject Player1Active;
+public GameObject Player2Active;
+public GameObject Player1Inactive;
+public GameObject Player2Inactive;
+
+bool player1IsReady = false;
+bool player2IsReady = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1Inactive.SetActive(!player1IsReady);
+        Player2Inactive.SetActive(!player2IsReady);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)){
+            player1IsReady = !player1IsReady;
+            Player1Active.SetActive(player1IsReady);
+            Player1Inactive.SetActive(!player1IsReady);
+        } else if (Input.GetKeyDown(KeyCode.D)){
+            player2IsReady = !player2IsReady;
+            Player2Active.SetActive(player2IsReady);
+            Player2Inactive.SetActive(!player2IsReady);
+        }
+        if (player1IsReady && player2IsReady){
+            SceneManager.LoadScene(1);
+        }
+        
+    }
+}
