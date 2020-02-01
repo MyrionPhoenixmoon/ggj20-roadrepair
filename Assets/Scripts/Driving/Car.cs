94798204c7907ed321c52ba883d330afd2100acc
jonathan.spielvogel <jonathan.spielvogel@adesso.ch>
2020-02-01 19:53:02 +0100

    Added stiffening of turning related to speed. Also Code cleanup

--- Assets/Scripts/Driving/Car.cs
+++ Assets/Scripts/Driving/Car.cs
@@ -23,7 +23,62 @@
 
         public Rigidbody RigidBody { get; private set; }
 
-        public float Speed { get; private set; }
+        public int Speed { get; private set; }
+
+        private void ApplyDirections(float acceleration, bool isBreaking, float turningDirection)
+        {
+            if (Math.Abs(acceleration) > .3f)
+            {
+                var velocity = this.RigidBody.velocity.magnitude;
+
+                this.RigidBody.maxAngularVelocity = AngularVelocityCap - (float)Math.Log(velocity);
+
+                this.RigidBody.velocity = isBreaking ? this.RigidBody.velocity * 0.9f : this.RigidBody.velocity;
+
+                this.RigidBody.AddRelativeForce(0, 0, acceleration * 150);
+                this.RigidBody.AddRelativeTorque(0, turningDirection * 100, 0);
+            }
+            else
+            {
+                if (Math.Abs(turningDirection) > .1f)
+                {
+                    var inertiaDampener = new Vector3(
+                        this.RigidBody.velocity.x * InertiaDampenerFactor,
+                        this.RigidBody.velocity.y * InertiaDampenerFactor,
+                        this.RigidBody.velocity.z * InertiaDampenerFactor);
+                    this.RigidBody.AddForce(inertiaDampener);
+                    this.RigidBody.AddRelativeTorque(0, turningDirection * 100, 0);
+                    this.RigidBody.AddRelativeForce(
+                        new Vector3(turningDirection * this.RigidBody.velocity.magnitude, 0, 0));
+                }
+            }
+        }
+
+        private void CapAtMaxSpeed()
+        {
+            if (this.RigidBody.velocity.magnitude > this.maxSpeed)
+            {
+                var breakingForce = new Vector3(
+                    this.RigidBody.velocity.x * BreakingFactor,
+                    this.RigidBody.velocity.y * BreakingFactor,
+                    this.RigidBody.velocity.z * BreakingFactor);
+                this.RigidBody.AddForce(breakingForce);
+            }
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            this.GatherAxisValues();
+
+            var acceleration = this.GatherAcceleration();
+            var turningDirection = this.GatherTurningDirection();
+            var isBreaking = this.GatherBreaking();
+
+            this.ApplyDirections(acceleration, isBreaking, turningDirection);
+            this.CapAtMaxSpeed();
+            this.SetSpeed();
+        }
 
         private float GatherAcceleration()
         {
@@ -66,12 +121,17 @@
             return turningDirection;
         }
 
+        private void SetSpeed()
+        {
+            this.Speed = (int)(this.RigidBody.velocity.magnitude * 10);
+        }
+
         // Start is called before the first frame update
         private void Start()
         {
             this.RigidBody = this.gameObject.GetComponent<Rigidbody>();
 
-            this.RigidBody.maxAngularVelocity = 2;
+            this.RigidBody.maxAngularVelocity = AngularVelocityCap;
             this.maxSpeed = 30;
 
             this.currentForces = new Dictionary<string, float>();
@@ -80,49 +140,6 @@
             {
                 this.axis.Add(((Axis)entry).AsString(EnumFormat.Description));
             }
-        }
-
-
-        // Update is called once per frame
-        private void FixedUpdate()
-        {
-            this.GatherAxisValues();
-
-            var acceleration = this.GatherAcceleration();
-            var turningDirection = this.GatherTurningDirection();
-            var isBreaking = this.GatherBreaking();
-            if (Math.Abs(acceleration) > .3f)
-            {
-                this.RigidBody.velocity = isBreaking ? this.RigidBody.velocity * 0.9f : this.RigidBody.velocity;
-
-                this.RigidBody.AddRelativeForce(0, 0, acceleration * 150);
-                this.RigidBody.AddRelativeTorque(0, turningDirection * 100, 0);
-            }
-            else
-            {
-                if (Math.Abs(turningDirection) > .1f)
-                {
-                    var inertiaDampener = new Vector3(
-                        this.RigidBody.velocity.x * InertiaDampenerFactor,
-                        this.RigidBody.velocity.y * InertiaDampenerFactor,
-                        this.RigidBody.velocity.z * InertiaDampenerFactor);
-                    this.RigidBody.AddForce(inertiaDampener);
-                    this.RigidBody.AddRelativeTorque(0, turningDirection * 100, 0);
-                    this.RigidBody.AddRelativeForce(new Vector3(turningDirection * this.RigidBody.velocity.magnitude, 0, 0));
-                }
-            }
-
-            if (this.RigidBody.velocity.magnitude > this.maxSpeed)
-            {
-                var breakingForce = new Vector3(
-                    this.RigidBody.velocity.x * BreakingFactor,
-                    this.RigidBody.velocity.y * BreakingFactor,
-                    this.RigidBody.velocity.z * BreakingFactor);
-                this.RigidBody.AddForce(breakingForce);
-            }
-
-            Debug.Log(this.RigidBody.velocity.magnitude.ToString());
-            this.Speed = this.RigidBody.velocity.magnitude * 10;
         }
     }
 }
