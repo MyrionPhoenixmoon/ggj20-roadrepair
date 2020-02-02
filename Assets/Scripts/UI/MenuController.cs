f40ca39bcc9d7f41e87e6c9cdc1a0e90eaf6657c
Mark Zeman <zemanmark@hotmail.com>
2020-02-02 14:20:45 +0100

    Updated losescreen and improved behaviour of startscreen

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -13,31 +13,44 @@ public GameObject Player2Inactive;
 
 bool player1IsReady = false;
 bool player2IsReady = false;
+bool gameStarting = false;
+
+public float startDelay;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Player1Inactive.SetActive(!player1IsReady);
-        Player2Inactive.SetActive(!player2IsReady);
+        this.Player1Inactive.SetActive(!player1IsReady);
+        this.Player2Inactive.SetActive(!player2IsReady);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)){
-            player1IsReady = !player1IsReady;
-            Player1Active.SetActive(player1IsReady);
-            Player1Inactive.SetActive(!player1IsReady);
-        } else if (Input.GetKeyDown(KeyCode.D)){
-            player2IsReady = !player2IsReady;
-            Player2Active.SetActive(player2IsReady);
-            Player2Inactive.SetActive(!player2IsReady);
+        if (Input.GetKeyDown(KeyCode.A) && !this.gameStarting){
+
+            this.player1IsReady = !this.player1IsReady;
+            this.Player1Active.SetActive(this.player1IsReady);
+            this.Player1Inactive.SetActive(!this.player1IsReady);
+
+        } else if (Input.GetKeyDown(KeyCode.D) && !this.gameStarting){
+
+            this.player2IsReady = !this.player2IsReady;
+            this.Player2Active.SetActive(this.player2IsReady);
+            this.Player2Inactive.SetActive(!this.player2IsReady);
+
         }
-        if (player1IsReady && player2IsReady){
-            SceneManager.LoadScene(1);
+
+        if (this.player1IsReady && this.player2IsReady){
+            this.gameStarting = true;
+            Invoke("startGame", this.startDelay);            
         }
         
+    }
+
+    void startGame(){
+        SceneManager.LoadScene(1);
     }
 }
